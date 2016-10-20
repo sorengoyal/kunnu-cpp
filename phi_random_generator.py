701ddd66c49016e9aac4ec8ea88384eef869a077
Soren Goyal <sorengoyal@gmail.com>
2016-10-19 23:44:26 -0400

    changed the values of cohesion and phi to the correct ones

--- phi_random_generator.py
+++ phi_random_generator.py
@@ -1,5 +1,5 @@
 #To run 
-#python rho_random_generator 10
+#python phi_random_generator 10
 
 import sys
 import numpy as np
@@ -8,7 +8,7 @@ N = int(sys.argv[1])#Get the first command line argument
 
 while (N > 0):
 	n = np.random.randn(1)[0] #Genrate a single random number
-	final_no = 1750 + n*50 #Scale to correct mean and s.dev
+	final_no = 0.35 + n*0.05 #Scale to correct mean and s.dev
 	if(final_no >= 0):
 		print(final_no) 
 		N = N - 1
