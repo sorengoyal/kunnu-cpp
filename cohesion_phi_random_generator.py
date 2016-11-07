d115c25cd6ad56c83dbf9dd198585d52cfb2d65a
sorengoyal <sorengoyal@gmail.com>
2016-11-07 16:20:43 -0500

    Update cohesion_phi_random_generator.py

--- cohesion_phi_random_generator.py
+++ cohesion_phi_random_generator.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 
 N = int(sys.argv[1])#Get the first command line argument
+f = open('cohesion_phi_n1_n2.dat', 'w')
 
 while (N > 0):
 	n = np.random.randn(1)[0] #Genrate a single random number
@@ -13,6 +14,7 @@ while (N > 0):
 	phi = 0.35 + n*0.05 #Scale to correct mean and s.dev
 	if(cohesion >= 0 and phi >= 0):
 		print(cohesion, phi)
+		f.write("%.3f, %.3f, %.3f, %.3f\n" % (cohesion, phi, n1, n2))
 		N = N - 1
 	
 	
