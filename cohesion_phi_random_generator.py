50ed7e2e18dc504d2b580cc9252558c1582bf8dd
sorengoyal <sorengoyal@gmail.com>
2016-11-12 16:51:25 -0500

    Update cohesion_phi_random_generator.py
    From now on, iterator.sh will not be needed. cohesion_phi_random_generator.py will 
    - generate 'N' combinations of random variables cohesion and phi, save them to a file cohesion_phi.dat
    - the random rumber n1 and n2 will also be saved to xi_n.dat. (though i do not know what this would be useful for)
    - execute the code demo wave trial pulse

--- cohesion_phi_random_generator.py
+++ cohesion_phi_random_generator.py
@@ -1,19 +1,19 @@
-#To run 
-#python E_random_generator 10
-
 import sys
 import numpy as np
+import os
 
 N = int(sys.argv[1])#Get the first command line argument
 f = open('xi_n.dat', 'w')
-
+g = open('cohesion_phi.dat','w')
 while (N > 0):
 	n1 = np.random.randn(1)[0] #Genrate a single random number
 	cohesion = 25000 + n1*2000 #Scale to correct mean and s.dev
 	n2 = np.random.randn(1)[0]
 	phi = 0.35 + n2*0.05 #Scale to correct mean and s.dev
 	if(cohesion >= 0 and phi >= 0):
-		print("%.3f %.3f" % (cohesion, phi))
+		command = './demo_wavetrial_pulseVM ' + str(cohesion) + ' ' + str(phi)
+		os.system(command)
+		g.write("%.3f %.3f" % (cohesion, phi))
 		f.write("%.3f, %.3f\n" % (n1, n2))
 		N = N - 1	
 	
